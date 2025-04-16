jcaesar/lls

commit b00dcdac800189933157daa96f1a250170a57aeb

Duplicity is noise
This reverts commit 35770111bf3871b5a6a89576105a68edb1933e0a.

src/main.rs
@@ -12,7 +12,7 @@ use netlink::{
 };
 use procfs::process::all_processes;
 use std::{
-    collections::{BTreeMap, HashMap},
+    collections::{BTreeMap, BTreeSet, HashMap},
     env::var_os,
     io::{stdout, BufWriter, Write},
     net::{IpAddr, Ipv4Addr, Ipv6Addr},
@@ -184,25 +184,16 @@ fn sockets_tree<'a>(
         {
             sout.leaf("0.0.0.0 + ::".into());
         } else {
-            let fields = |sock: &SockInfo<'a>| (sock.family, sock.iface, sock.addr);
-            let mut counts = socks
+            let socks = socks
                 .iter()
                 .filter(|sock| filter.accept_addr(sock.addr))
-                .counts_by(|sock| fields(sock));
+                .collect::<BTreeSet<_>>();
             for sock in socks {
-                let count = counts.remove(&fields(&*sock));
-                if let Some(count) = count {
-                    let disp = match (sock.family, sock.iface) {
-                        (Family::Both, _) => "*".into(),
-                        (_, Some(ifname)) => format!("{} ({ifname})", sock.addr),
-                        _ => format!("{}", sock.addr),
-                    };
-                    let duplicity = match count {
-                        1 => "".into(),
-                        _ => format!(" (×{count})"),
-                    };
-                    sout.leaf(format!("{disp}{duplicity}"));
-                }
+                match (sock.family, sock.iface) {
+                    (Family::Both, _) => sout.leaf("*".into()),
+                    (_, Some(ifname)) => sout.leaf(format!("{} ({ifname})", sock.addr)),
+                    _ => sout.leaf(format!("{}", sock.addr)),
+                };
             }
         }
         if filter.accept_port(port) && filter.accept_proto(proto) {

src/netlink/sock.rs
@@ -68,7 +68,7 @@ pub fn all_sockets<'i>(
     Ok(ret)
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub enum Family {
     V4,
     V6,

commit 7e633e20037bbace3908488e7a586081829f1312

Fix deduplication
(Deduplication reverted to the previous broken state in b00dcda.)

src/main.rs
@@ -187,12 +187,13 @@ fn sockets_tree<'a>(
             let socks = socks
                 .iter()
                 .filter(|sock| filter.accept_addr(sock.addr))
+                .map(|sock| (sock.family, sock.addr, sock.iface))
                 .collect::<BTreeSet<_>>();
-            for sock in socks {
-                match (sock.family, sock.iface) {
+            for (family, addr, iface) in socks {
+                match (family, iface) {
                     (Family::Both, _) => sout.leaf("*".into()),
-                    (_, Some(ifname)) => sout.leaf(format!("{} ({ifname})", sock.addr)),
-                    _ => sout.leaf(format!("{}", sock.addr)),
+                    (_, Some(ifname)) => sout.leaf(format!("{} ({ifname})", addr)),
+                    _ => sout.leaf(format!("{}", addr)),
                 };
             }
         }